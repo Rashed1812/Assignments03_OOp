67561dd79307c70ccc1776095acb689a87187eb5
Your Name <you@example.com>
2025-01-16 14:49:08 +0200

    Add project files.

---
+++ Assignments03_OOp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignments03_OOp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
